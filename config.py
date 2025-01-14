3f85e671f2bf17cb566f9ffb925fea8bb2d86a5e
Shokhrukh Tolipov <55028895+Shorux@users.noreply.github.com>
2025-01-14 13:29:20 +0500

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,11 @@ import os
 import logging
 
 from dotenv import load_dotenv
-from distutils.util import strtobool
 
 
 load_dotenv()  # parse environment variables from .env file
 
-DEBUG = bool(strtobool(os.getenv('DEBUG', 'off')))
-
+DEBUG = os.getenv('DEBUG', False)
 BOT_TOKEN = os.getenv('BOT_TOKEN')
 if not BOT_TOKEN:
     logging.error("BOT_TOKEN is not defined neither in .env file nor in environment variables")
